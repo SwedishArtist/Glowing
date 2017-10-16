c7ab8444d7a9ba06bff9d20751d63809ca5a1083
SwedishArtist <32638426+SwedishArtist@users.noreply.github.com>
2017-10-16 13:07:08 +0200

    Problem #1

---
+++ index.js
@@ -0,0 +1,72 @@
+// Omslut all kod med en 
+// self-executing (self invoking)
+// anonymous function
+(() => {
+  'use strict';
+  let canvas = document.getElementById('glowcanvas');
+  let context = canvas.getContext('2d');
+  
+  canvas.width = window.innerWIdth;
+  canvas.height = window.innerHeight - 10;
+  // Scrollbars uppträder om hela höjden utnyttjas
+  
+  // startvärden första kurvan
+  let firstX = context.witdh * Math.random();
+  let firstY = context.height * Math.random();
+  // Färg väljs ut "färghjul", första värdet utgår från detta
+  let initHue - 0;
+  
+  /**
+  * Funktionen som ritar linjerna
+  * 
+  * @param curX Där kurvan ska börja = där den slutade sist
+  * @param curX Där kurvan ska börja = där den slutade sist
+  * @param hue Senast använda nyans (HSL)
+  */
+  function line(curX, curY, hue) {
+    // Sparar "tillståndet" = onödigt här men god praxis
+    context.save();
+    // Påbörja en ny kurva
+    context.beginPath();
+    // Bredd mellan 5 och 15
+    context.lineWidth = 5 + Math.random() * 10;
+    // Gå till startpunkt i koordinatsystemet = där förra kurvan slutade
+    context.moveTo(curX, curY);
+    // Slumpa fram en ny slutpunkt
+    curX = context.canvas.width * Math.random();
+    curY = context.canvas.height * Math.random();
+    // "Beställ" en kurva med slumpmässig böjning
+    context.bezierCurveTo(context.canvas.width  * Math.random(),
+                          context.canvas.height * Math.random(),
+                          context.canvas.width  * Math.random(),
+                          context.canvas.height * Math.random(),
+                          curX, curY);
+    
+    // Gå till slumpmässigt antal grader framåt i färghjulet
+    hue += 10 * Math.random();
+    context.strokeStyle = 'hsl(' + ', 50%, 50%';
+    // Uppgift (längre fram) - flytta ut shadowColor och shadowBlur
+    // eftersom de aldrig ändras
+    // Glödande kanlinje på kurvan
+    context.shadowColor = 'white';
+    context.shadowBlur  = 10;
+    // Genomför själva ritandet
+    context.stroke();
+    // Återställ gamla tillståndet
+    context.restore();
+    // Rekursiv anrop = anropa sig själv
+    setTimeout(() => { line(curX, curY, hue); }, 80;
+  }
+    // Första anropet av funktionen
+    line(firstX, firstY, initHue);
+    
+    // Skapa en helsvart men mycket transparent "hinna" som läggs överst,
+    // men gradvis flyttas bakåt
+    function blank() {
+      // Svart med 90% genomskinlighet
+      context.fillStyle = 'rgba(0, 0, 0, 0.1)';
+      // Rektangel med fyllning
+      context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+    }
+    setInterval(blank, 100);
+})();
